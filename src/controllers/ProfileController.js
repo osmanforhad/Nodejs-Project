01f0460a2ea8bfc695a9f4d37a9917f48bff99e3
osmanforhad <developer.osmanforhad@gmail.com>
2022-07-10 22:50:38 +0600

    working with user login api and login with a screate token

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,4 +1,5 @@
 const ProfileModel = require("../models/ProfileModel");
+var jwt = require('jsonwebtoken');
 
 exports.CreateProfile = (request, response) => {
     let reqbody = request.body;
@@ -10,4 +11,30 @@ exports.CreateProfile = (request, response) => {
             response.status(200).json({status: "success", data:data});
         }
     });
+}
+
+exports.UserLogin = (request, response) => {
+    let UserName = request.body['UserName'];
+    let Password = request.body['Password'];
+    ProfileModel.find({UserName:UserName, Password:Password}, (error, data) => {
+        if(error){
+            response.status(400).json({status: "fail", data:error});
+        }
+        else{
+            if(data.length>0){
+
+                //Create Auth Token
+                let Payload = {
+                    exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
+                    data:data[0]
+                } 
+                let token = jwt.sign(Payload, 'SecretKey123456789');
+                response.status(200).json({status: "success", token: token, data:data[0]});
+            }
+            else{
+                response.status(401).json({status: "Unauthorized"});
+            }
+        }
+    });
+
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -4,6 +4,7 @@ const ProfileController = require('../controllers/ProfileController');
 const router = express.Router();
 
 router.post("/CreateProfile", ProfileController.CreateProfile);
+router.post("/UserLogin", ProfileController.UserLogin);
 
 
 module.exports = router;
