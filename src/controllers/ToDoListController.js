a9fec9a6182c4ac3260ba05112b6a51d75569285
osmanforhad <developer.osmanforhad@gmail.com>
2022-07-11 18:10:42 +0600

    working with update todo list api

--- src/controllers/ToDoListController.js
+++ src/controllers/ToDoListController.js
@@ -41,3 +41,26 @@ exports.SelectToDo = (request, response) => {
     });
 
 }
+
+exports.UpdateToDo = (request, response) => {
+   let ToDoSubject = request.body['ToDoSubject'];
+   let ToDoDescription = request.body['ToDoDescription'];
+   let _id = request.body['_id'];
+   let ToDoUpdateDate = Date.now();
+
+   let PostBody = {
+    ToDoSubject: ToDoSubject,
+    ToDoDescription: ToDoDescription,
+    ToDoUpdateDate: ToDoUpdateDate
+   }
+
+   ToDoListModel.updateOne({_id:_id}, {$set:PostBody}, {upsert:true}, (error, data) => {
+    if(error) {
+        response.status(400).json({status: "fail", data:error});
+    }
+    else{
+        response.status(200).json({status: "success", data:data});
+    }
+   });
+
+}
--- src/routes/api.js
+++ src/routes/api.js
@@ -13,6 +13,7 @@ router.post("/UpdateProfile", AuthVerifyMiddleware, ProfileController.UpdateProf
 
 router.post("/CreateToDo", AuthVerifyMiddleware, ToDoListController.CreateToDo);
 router.get("/SelectToDo", AuthVerifyMiddleware, ToDoListController.SelectToDo);
+router.post("/UpdateToDo", AuthVerifyMiddleware, ToDoListController.UpdateToDo);
 
 
 module.exports = router;
